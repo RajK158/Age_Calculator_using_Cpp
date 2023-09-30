cdecd34410ac6cdab0af7bfae4da72972e7254a6
Raj Kundur <94987579+RajK158@users.noreply.github.com>
2023-09-30 01:00:22 -0700

    Add files via upload

---
+++ agecalc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include <string>
+int main(int argc, char const *argv[])
+{
+    
+   int ageinyears;
+    int ageindays;
+   cout<<"Enter your age: ";
+   cin>>ageinyears;
+   ageindays=ageinyears*365;
+   cout<<"age in days is "<<ageindays<<endl;
+    return 0;
+}
+
